01ad237c5e858422b94108c148c612d61d2f0380
pabloernestomartinez-collab <pabloernestomartinez@upc.edu.ar>
2025-11-24 21:01:34 -0300

    correcciones con rider

--- Assets/script/management.cs
+++ Assets/script/management.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class management : MonoBehaviour
 {
-    private int _vidasPlayer;
-    private int _vidasEnemigo;
-    private int _vacasEncerradas;
-    private int _torosEncerrados;
+    //private int _vidasPlayer;
+    //private int _vidasEnemigo;
+    //private int _vacasEncerradas;
+    //private int _torosEncerrados;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _vidasEnemigo = 10;
-        _vidasPlayer = 10;
-        _vacasEncerradas = 0;
-        _torosEncerrados = 0;
+       // _vidasEnemigo = 10;
+       // _vidasPlayer = 10;
+       // _vacasEncerradas = 0;
+       // _torosEncerrados = 0;
 
     }
 

4daedbe4c31a4ccefa284baa7434ae8c9fd4c930
pabloernestomartinez-collab <pabloernestomartinez@upc.edu.ar>
2025-11-29 14:05:05 -0300

    agregado de cucha

--- Assets/script/perro.cs
+++ Assets/script/perro.cs
@@ -5,7 +5,11 @@ public class perro : MonoBehaviour
 {
     private Rigidbody _rb;
     [SerializeField] private Transform _objetive;
+    [SerializeField] private Transform _cucha;
+    private Transform _cambio;
+
     private NavMeshAgent _agent;
+
 
     private void Start()
     {
@@ -25,8 +29,9 @@ public class perro : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Toro"))//    tag == "toro" mata al perro
         {
-            Destroy(gameObject); //destruye el perro
-
+            _cambio = _objetive;
+            _objetive = _cucha;
+            _cucha = _cambio;
         }
     }
 
